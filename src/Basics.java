e0b476a4b432bd6a2f311f873331e3a0d3667cbe
Kamla <kamla.21bisht@gmail.com>
2020-04-29 22:49:11 +0100

    first commit

---
+++ src/Basics.java
@@ -0,0 +1,16 @@
+
+public class Basics {
+	
+	static int myMethod (int x, int y) {
+		return x+y;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Result is " + myMethod (3,5));
+
+	}
+
+}
---
+++ src/Parent.java
@@ -0,0 +1,12 @@
+
+public class Parent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Methods m = new Methods();
+		m.ValidateHeader();
+
+	}
+
+}
